a0d7fe855d5389dfa2de477ff9e8df578b597136
anvieira <anterofvfernandes@gmail.com>
2024-07-25 02:24:31 +0100

    correcções no all

--- backupmg.py
+++ backupmg.py
@@ -1,6 +1,8 @@
 #app para a criação de backups de bases de dados MongoDB
-import os
 import subprocess
+from os import getenv
+from sys import exit, argv
+import os
 import sys
 
 # os.environ['DATABASE_USERNAME'] = 'antero'
@@ -13,14 +15,15 @@ def error_message():
     if (len(sys.argv) > 1):
         print('comando inválido, utiliza: "./backupmg"')
         sys.exit(1)
-    if os.getenv('DATABASE_USERNAME') is None or os.getenv('DATABASE_PASSWORD') is None or os.getenv('DATABASE_CONNECT_STRING') is None or os.getenv('BD_DATABASE_NAME') is None:
+    
+    if not all(os.getenv(var) for var in ['DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_CONNECT_STRING', 'BD_DATABASE_NAME']):
         print('Variáveis de ambiente não configuradas, utiliza: export DATABASE_USERNAME=<username>, export DATABASE_PASSWORD=<password>, export DATABASE_CONNECT_STRING=<url>, export BD_DATABASE_NAME=<database>')
         sys.exit(1)
 
 
 def make_backup(db_name):
-    backup = os.getenv('BACKUP_DIR') if os.getenv('BACKUP_DIR').strip() != "" \
-        or os.getenv('BACKUP_DIR') is None  else "dump"
+    backup = "dump" if not os.getenv('BACKUP_DIR') or os.getenv('BACKUP_DIR').strip() == "" else os.getenv('BACKUP_DIR')
+    
     command = [
         'mongodump',
         '--host', os.getenv('DATABASE_CONNECT_STRING'),
@@ -32,8 +35,10 @@ def make_backup(db_name):
     try:
         subprocess.run(command, check=True)
         print('Backup realizado com sucesso, verifique a pasta: ' + backup)
+
     except subprocess.CalledProcessError as e:
         print('Falha ao realizar o backup:', e)
+
     except KeyboardInterrupt:
         print('Backup cancelado pelo usuário')
 
@@ -41,5 +46,6 @@ if __name__ == "__main__":
     error_message()
     try:
         make_backup(os.getenv('BD_DATABASE_NAME'))
+
     except Exception as e:
         print('Falha ao conectar ao MongoDB:', e)
